6158593bc8c5e40545f85abe3287ca0979493119
ChristopherAsogwa <christopherasogwa8@gmail.com>
2024-12-15 00:23:29 +0100

    Fix possible errors

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -13,7 +13,6 @@ interface HomeCardProps {
 
 const HomeCard = ({ className, img, title, description, handleClick }: HomeCardProps) => {
   return (
-      // eslint-disable-next-line jsx-a11y/no-static-element-interactions
       <section
           className={cn(
               'bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer',
